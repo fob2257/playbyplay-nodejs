2fa498accea3eed463966afc6a7ffcfecbb8ccdd
fob2257 <fob2257@gmail.com>
2017-08-08 13:16:01 -0700

    event loop example

--- routes/pet.routes.js
+++ routes/pet.routes.js
@@ -9,6 +9,33 @@ var client = redis.createClient(6379, '127.0.0.1');
 module.exports = function (app) {
     app.get('/pets', (req, res) => {
         async.parallel({
+            dog: (callback) => {
+                client.get('http://localhost:3001/dog', (error, dog) => {
+                    if (error) {
+                        throw error
+                    }
+                    if (dog) {
+                        callback(null, JSON.parse(dog))
+                    } else {
+                        r('http://localhost:3001/dog', function (error, response, body) {
+                            if (error) {
+                                callback({ service: 'dog', error: error })
+                                return
+                            }
+                            if (!error && response.statusCode === 200) {
+                                callback(null, body)
+                                client.setex('http://localhost:3001/dog', 30, JSON.stringify(body), (error) => {
+                                    if (error) {
+                                        throw error
+                                    }
+                                })
+                            } else {
+                                callback(response.statusCode)
+                            }
+                        });
+                    }
+                })
+            },
             cat: (callback) => {
                 client.get('http://localhost:3000/cat', (error, cat) => {
                     if (error){throw error}
@@ -43,33 +70,6 @@ module.exports = function (app) {
                 //         callback(response.statusCode)
                 //     }
                 // });
-            },
-            dog: (callback) => {
-                client.get('http://localhost:3001/dog', (error, dog) => {
-                    if (error) {
-                        throw error
-                    }
-                    if (dog) {
-                        callback(null, JSON.parse(dog))
-                    } else {
-                        r('http://localhost:3001/dog', function (error, response, body) {
-                            if (error) {
-                                callback({ service: 'dog', error: error })
-                                return
-                            }
-                            if (!error && response.statusCode === 200) {
-                                callback(null, body)
-                                client.setex('http://localhost:3001/dog', 30, JSON.stringify(body), (error) => {
-                                    if (error) {
-                                        throw error
-                                    }
-                                })
-                            } else {
-                                callback(response.statusCode)
-                            }
-                        });
-                    }
-                })
             }
         },
             function (error, results) {
@@ -81,6 +81,6 @@ module.exports = function (app) {
         )
     })
     app.get('/ping', (req, res) => {
-        res.json({ 'pong': Data.now() })
+        res.json({ 'pong': Date.now() })
     })
 }
