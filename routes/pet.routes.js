52cf4acc16da25ecc3a78dafb62958912a4944ce
fob2257 <fob2257@gmail.com>
2017-08-08 11:37:43 -0700

    added redis get to cat block

--- routes/pet.routes.js
+++ routes/pet.routes.js
@@ -10,17 +10,39 @@ module.exports = function (app) {
     app.get('/pets', (req, res) => {
         async.parallel({
             cat: (callback) => {
-                r('http://localhost:3000/cat', function (error, response, body) {
-                    if (error) {
-                        callback({ service: 'cat', error: error })
-                        return
-                    }
-                    if (!error && response.statusCode === 200) {
-                        callback(null, body)
+                client.get('http://localhost:3000/cat', (error, cat) => {
+                    if (error){throw error}
+                    if (cat) {
+                        callback(null, JSON.parse(cat))
                     } else {
-                        callback(response.statusCode)
+                        r('http://localhost:3000/cat', function (error, response, body) {
+                            if (error) {
+                                callback({ service: 'cat', error: error })
+                                return
+                            }
+                            if (!error && response.statusCode === 200) {
+                                callback(null, body)
+                                client.set('http://localhost:3000/cat', JSON.stringify(body), (error) => {
+                                    if (error){throw error}
+                                })
+                            } else {
+                                callback(response.statusCode)
+                            }
+                        });
                     }
-                });
+                })
+
+                // r('http://localhost:3000/cat', function (error, response, body) {
+                //     if (error) {
+                //         callback({ service: 'cat', error: error })
+                //         return
+                //     }
+                //     if (!error && response.statusCode === 200) {
+                //         callback(null, body)
+                //     } else {
+                //         callback(response.statusCode)
+                //     }
+                // });
             },
             dog: (callback) => {
                 client.get('http://localhost:3001/dog', (error, dog) => {
