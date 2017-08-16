6c3cd933e4ea06a2d57a15ddc9b393ac786e6b37
fob2257 <fob2257@gmail.com>
2017-08-16 00:11:13 -0700

    Login with passport on GET /pets

--- pet.server.js
+++ pet.server.js
@@ -1,13 +1,27 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-
+var passport = require('passport')
+var mongoose = require('mongoose')
 var app = express()
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+var options = {
+    useMongoClient: true,
+    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
+};
+mongoose.connect('mongodb://localhost/users', options)
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }))
 
+var localStrategy = require('./lib/strategy.js')
 var petRoutes = require('./routes/pet.routes.js')(app)
+var errors = require('./lib/errors')(app)
 
 var server = app.listen(3002, () => {
     console.log('pet.server running on port 3002')
--- routes/pet.routes.js
+++ routes/pet.routes.js
@@ -1,5 +1,7 @@
 var async = require('async')
 var redis = require('redis')
+var passport = require('passport')
+var authorize = require('../lib/authorize.js')()
 var r = require('request').defaults({
     json: true
 })
@@ -7,7 +9,7 @@ var r = require('request').defaults({
 var client = redis.createClient(6379, '127.0.0.1');
 
 module.exports = function (app) {
-    app.get('/pets', (req, res) => {
+    app.get('/pets', authorize.authorize, (req, res) => {
         async.parallel({
             dog: (callback) => {
                 client.get('http://localhost:3001/dog', (error, dog) => {
@@ -38,7 +40,7 @@ module.exports = function (app) {
             },
             cat: (callback) => {
                 client.get('http://localhost:3000/cat', (error, cat) => {
-                    if (error){throw error}
+                    if (error) { throw error }
                     if (cat) {
                         callback(null, JSON.parse(cat))
                     } else {
@@ -50,7 +52,7 @@ module.exports = function (app) {
                             if (!error && response.statusCode === 200) {
                                 callback(null, body)
                                 client.set('http://localhost:3000/cat', JSON.stringify(body), (error) => {
-                                    if (error){throw error}
+                                    if (error) { throw error }
                                 })
                             } else {
                                 callback(response.statusCode)
@@ -77,10 +79,18 @@ module.exports = function (app) {
                     error: error,
                     results: results
                 })
-            }
-        )
+            })
     })
-    app.get('/ping', (req, res) => {
-        res.json({ 'pong': Date.now() })
-    })
+    app.post('/signup', passport.authenticate('local-signup'), signupPost)
+    app.post('/login', passport.authenticate('local-login'), loginPost)
+    app.get('/ping', authorize.authorize, ping)
+}
+ping = function (req, res) {
+    res.json({ pong: true });
+}
+signupPost = function (req, res) {
+    authorize.sendToken(req, res);
+}
+loginPost = function (req, res) {
+    authorize.sendToken(req, res);
 }
